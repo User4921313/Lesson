5a6374397a73442c2f9138bcb16fd4358fb8a910
User4921313 <krivosheeva190715@gmail.com>
2021-04-29 13:57:33 +0700

    form+index js2

--- asset/js/main.js
+++ asset/js/main.js
@@ -25,7 +25,7 @@ function changeColor()  {
 
     console.log(colorArray[i])
 
-    if (i> colorArray.length  -1) {
+    if (i > colorArray.length  -1) {
         i = 0;
     }
 }
@@ -34,10 +34,53 @@ function selectColor() {
   let color = document.getElementById("color").value;
   document.body.style.background = color;
 
-  document.getElementById('result').innerHTML = color;
+  document.getElementById('result').innerHTML = color;  
 } 
 
   let oper_elements = document.getElementsByClassName('oper');
+  let numbers = document.getElementsByClassName('number')
 
-  console.log(oper_elements);
+for (var i = 0; i < oper_elements.length; i++) {
+oper_elements[i].addEventListener('click',oper);
+}
+
+for (var i = 0; i < numbers.length; i++) {
+numbers[i].addEventListener('click', addNumber);
+}
+
+ function oper() {
+  let o = this.value;
+  console.log(o);
+
+  add(o);
+
+  if (o == '=') {
+    // alert(o);
+    // calc();
+  }
+ }
+
+function addNumber(){
+  let n = this.value;
+
+  add(n);
+}
+
+
+ function add(v) {
+  let input = document.getElementById('enter');
+
+  let output = input.value;
+
+  input.value = output + v;
+ }
+
+
+// for (var i = 0; i < oper_elements.length; i++) {
+// oper_elements[i].addEventListener('click', function(event) {
+// console.log(event)
+// });
+// }
+
+console.log(oper_elements);
 
